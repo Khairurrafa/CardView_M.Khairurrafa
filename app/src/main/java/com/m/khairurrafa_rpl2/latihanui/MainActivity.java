c93d0ffedd7e80d97dd6baa794dedfb96dafd55a
SMKN 4 BANDUNG <erlangga.rafa022@gmail.com>
2021-11-02 13:15:45 +0700

    Latihan1

---
+++ app/src/main/java/com/m/khairurrafa_rpl2/latihanui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.m.khairurrafa_rpl2.latihanui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
